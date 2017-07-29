596321d1a83d93a66bcc448fa233b2d0a9a617ab
ASWANTH P P <ppaswanth3@gmail.com>
2017-07-29 16:47:14 +0530

    Update Controllers.js

--- js/ng/Controllers.js
+++ js/ng/Controllers.js
@@ -1,4 +1,19 @@
 var app = angular.module('website', []);
 app.controller('GlobalController', function ($scope, $http, $window) {
-   
+   $scope.tab1=function(){
+       
+       $('#home')[0].scrollIntoView( true );
+   };
+   $scope.tab2=function(){
+  
+       $('#projects')[0].scrollIntoView( true );
+   };
+   $scope.tab3=function(){
+       
+        $('#internship')[0].scrollIntoView( true );
+   };
+   $scope.tab4=function(){
+      
+        $('#resume')[0].scrollIntoView( true );
+   };
 });
