0ede0e9cb87444dcda8a0fecdf7ae92966f088dc
ASWANTH P P <ppaswanth3@gmail.com>
2017-08-03 20:15:03 +0530

    Update Controllers.js

--- js/ng/Controllers.js
+++ js/ng/Controllers.js
@@ -2,7 +2,7 @@ var app = angular.module('website', []);
 app.controller('GlobalController', function ($scope, $http, $window) {
    $scope.tab1=function(){
        
-       $('#resume')[0].scrollIntoView( true );
+       $('#home')[0].scrollIntoView( true );
    };
    $scope.tab2=function(){
   
